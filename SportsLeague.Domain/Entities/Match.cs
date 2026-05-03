c4bd468e2bfd9fca7cd3e6dfac1ac6a75979586a
Juan David Ruiz <juanruiz164662@gmail.com>
2026-05-03 18:12:48 -0500

    Creation and complete flow of Match Result, Goals and Cards

--- SportsLeague.Domain/Entities/Match.cs
+++ SportsLeague.Domain/Entities/Match.cs
@@ -32,6 +32,8 @@ namespace SportsLeague.Domain.Entities
         public Team AwayTeam { get; set; } = null!;
 
         public Referee Referee { get; set; } = null!;
-
+        public MatchResult? MatchResult { get; set; }
+        public ICollection<Goal> Goals { get; set; } = new List<Goal>();
+        public ICollection<Card> Cards { get; set; } = new List<Card>();
     }
 }
